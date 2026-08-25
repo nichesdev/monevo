56a30331af9345b15f14a087ed1539bb351c9e87
retr0-r00t <enzoniches@gmail.com>
2026-08-25 10:49:32 -0300

    update

---
+++ monevo-wallet/src/main/java/br/com/nichesdev/monevo_wallet/MonevoWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.nichesdev.monevo_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonevoWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonevoWalletApplication.class, args);
+	}
+
+}
---
+++ monevo-wallet/src/test/java/br/com/nichesdev/monevo_wallet/MonevoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nichesdev.monevo_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonevoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
