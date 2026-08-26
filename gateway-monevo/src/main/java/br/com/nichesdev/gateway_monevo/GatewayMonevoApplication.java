dff122850014f002e566fc810e8e39ac378fd4f0
retr0-r00t <enzoniches@gmail.com>
2026-08-25 21:21:05 -0300

    gateway implementation

---
+++ gateway-monevo/src/main/java/br/com/nichesdev/gateway_monevo/GatewayMonevoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nichesdev.gateway_monevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMonevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMonevoApplication.class, args);
+	}
+
+}
---
+++ gateway-monevo/src/test/java/br/com/nichesdev/gateway_monevo/GatewayMonevoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nichesdev.gateway_monevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMonevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
