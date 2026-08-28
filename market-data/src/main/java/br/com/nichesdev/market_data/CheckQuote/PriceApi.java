66da73f2a503de9cf8cc68923c80b0a13b95247b
retr0-r00t <enzoniches@gmail.com>
2026-08-27 22:59:34 -0300

    Market data microservice

--- market-data/src/main/java/br/com/nichesdev/market_data/CheckQuote/PriceApi.java
+++ market-data/src/main/java/br/com/nichesdev/market_data/CheckQuote/PriceApi.java
@@ -1,20 +1,32 @@
 package br.com.nichesdev.market_data.CheckQuote;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("consulta-cotacao")
 public class PriceApi {
 
-    @GetMapping("{coin}")
+    @GetMapping("teste/{coin}")
     public CoinDto consultaMoeda(@PathVariable("coin") String coin) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<CoinDto> resp = restTemplate.getForEntity("https://economia.awesomeapi.com.br/json/last/{USD-BRL}?token=sk_uZyHPvEexGUpnrJae7omPX7OyDbJhnPyvWylWxQWAFzNJlEPaZ6EUnM1Yeem3", CoinDto.class);
-        return resp.getBody();
+        String url = "https://economia.awesomeapi.com.br/json/last/" + coin;
+
+        String jsonPuro = restTemplate.getForObject(url, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, CoinDto> mapaMoedas = mapper.readValue(jsonPuro, new TypeReference<Map<String, CoinDto>>() {});
+
+        if (mapaMoedas != null && !mapaMoedas.isEmpty()) {
+            return mapaMoedas.values().iterator().next();
+        }
+        return null;
     }
 }
